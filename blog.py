bec60969853acbda2eee21c5c21c8e695f29152a
zgj123 <1640851434@qq.com>
2018-09-20 21:36:11 +0800

    增加年份

--- blog.py
+++ blog.py
@@ -8,10 +8,29 @@ bp = Blueprint('blog', __name__)
 
 @bp.route("/tags/<keyword>/")
 def tags(keyword):
+    """
+    展示tags 部分的内容
+    :param keyword:
+    :return:
+    """
     return render_template('tags.html', keyword=keyword)
 
 
 @bp.route("/post/<post_path>/")
 def post(post_path):
+    """
+    展示文章
+    :param post_path:
+    :return:
+    """
     return render_template('post.html', post_path=post_path)
     pass
+
+@bp.route("/archives/<keyword>/")
+def archives(keyword):
+    """
+    展示Archives部分的内容
+    :param keyword:
+    :return:
+    """
+    return render_template('tags.html', keyword=keyword)
