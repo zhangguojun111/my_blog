762ea8842d435e91ce53026445f76b3c3ff0c0c6
zgj123 <1640851434@qq.com>
2018-09-26 09:00:19 +0800

    展示帖子，标签查询和时间查询

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template,session,redirect
 import blog
 import auth
 import admin
-
+import db
 
 app = Flask(__name__)
 app.secret_key = 'dey'
@@ -17,7 +17,12 @@ app.register_blueprint(admin.bp)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    select_sql = "select * from posts ORDER BY id DESC"
+    rows = db.query_db(select_sql)
+    select_sql = "select DISTINCT tag_name from posts_tags"
+    tag = db.query_db(select_sql)
+
+    return render_template("index.html", rows= rows,tag= tag)
 
 
 
--- blog.py
+++ blog.py
@@ -2,7 +2,7 @@
 处理登录，退出
 """
 from flask import Blueprint, render_template
-
+import db
 bp = Blueprint('blog', __name__)
 
 
@@ -13,7 +13,17 @@ def tags(keyword):
     :param keyword:
     :return:
     """
-    return render_template('tags.html', keyword=keyword)
+
+
+
+
+    select_sql = "SELECT * FROM posts WHERE id in (select post_id FROM posts_tags WHERE tag_name='"+keyword+"') ORDER BY id DESC"
+    rows = db.query_db(select_sql)
+    select_sql = "select DISTINCT tag_name from posts_tags"
+    tag = db.query_db(select_sql)
+
+
+    return render_template('tags.html', keyword=keyword,rows = rows, tag=tag)
 
 
 @bp.route("/post/<post_path>/")
@@ -23,7 +33,15 @@ def post(post_path):
     :param post_path:
     :return:
     """
-    return render_template('post.html', post_path=post_path)
+    select_sql = "select * from posts where url ='"+post_path+"'"
+    content = db.query_db(select_sql, one=True)
+
+
+    select_sql = "select DISTINCT tag_name from posts_tags"
+    tag = db.query_db(select_sql)
+
+
+    return render_template('post.html', post_path=content[4], tag=tag)
 
 
 @bp.route("/archives/<keyword>/")
@@ -33,4 +51,9 @@ def archives(keyword):
     :param keyword:
     :return:
     """
-    return render_template('tags.html', keyword=keyword)
+
+    select_sql = "SELECT * FROM posts WHERE ceated_at > '"+keyword+"-00-00 00:00:00 ' ORDER BY id DESC"
+    rows = db.query_db(select_sql)
+    select_sql = "select DISTINCT tag_name from posts_tags"
+    tag = db.query_db(select_sql)
+    return render_template('tags.html', keyword=keyword,tag=tag, rows=rows)
